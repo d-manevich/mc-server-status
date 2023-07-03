96d412f3fe1235f035ad2b0aad22733aadff3942
Daniil Manevich <daniil.manevich@gmail.com>
2023-07-03 23:20:21 +0800

    Fix protocol version. Remove last 24h

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 const TIMEOUT = 10000
-const PROTOCOL_VERSION = 756 // 1.7.1 from https://wiki.vg/Protocol_version_numbers
+const PROTOCOL_VERSION = process.env.PROTOCOL_VERSION || 763 // 1.7.1 from https://wiki.vg/Protocol_version_numbers
 const TOKEN = process.env.TG_TOKEN
+
+// Getting this ID from server if user is logging in
+const USER_MOCK_ID = "00000000-0000-0000-0000-000000000000"
 
 if (!TOKEN) throw new Error('You need to specify telegram bot token');
 
 const TelegramBot = require('node-telegram-bot-api')
 const mcping = require('mcping-js')
-const subDays = require('date-fns/subDays')
 
 const SERVERS_AND_CHATS_TO_NOTIFY = {} // { 'server url': [chatId, ...] }
 const CACHED_STATUSES = {} // { 'server url': { online: String, players: String } }
 const CHATS_MESSAGES = {} // { 'server url': { chatId: messageId } }
-const PLAYERS_TIME = {} // { 'server url': { name: time, ... } }
 
 console.log('init telegram bot')
 // Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(TOKEN, {polling: true});
+const bot = new TelegramBot(TOKEN, {polling: true})
 
 
 bot.setMyCommands([
@@ -26,7 +27,10 @@ bot.setMyCommands([
 
 function parseServerStatus(res) {
   const { players: { max, online, sample = [] } } = res
-  const playerList = sample.map(player => player.name).sort();
+  const playerList = sample
+    .filter(player => player.id !== USER_MOCK_ID)
+    .map(player => player.name)
+    .sort();
   const onlineInfo = `${online}/${max}`
   const playersInfo = playerList.join(', ')
 
@@ -38,25 +42,11 @@ function parseServerStatus(res) {
   }
 }
 
-function getLastPlayers(url) {
-  const allPlayersTime = PLAYERS_TIME[url] || {}
-  const allPlayersArr = Object.entries(allPlayersTime)
-  const yesterday = subDays(new Date(), 1);
-
-  return allPlayersArr.filter(([name, time]) => time > yesterday).map(([name]) => name)
-}
-
 function cacheStatus(url, online, players) {
   CACHED_STATUSES[url] = {
     online,
     players,
   }
-}
-
-function cachePlayersTime(url, playerList) {
-  if (!PLAYERS_TIME[url]) PLAYERS_TIME[url] = {}
-
-  playerList.forEach(player => PLAYERS_TIME[url][player] = new Date())
 }
 
 async function parseStartMsgUrl(msg) {
@@ -68,10 +58,11 @@ async function parseStartMsgUrl(msg) {
   }
   console.log('url entity found')
   const serverUrl = text.substring(urlEntity.offset, urlEntity.offset + urlEntity.length)
+  const [host, port] = serverUrl.split(':');
   try {
     await new Promise((resolve, reject) => {
       console.log(`try to connect to server ${serverUrl}`)
-      const serverTest = new mcping.MinecraftServer(serverUrl)
+      const serverTest = new mcping.MinecraftServer(host, port)
 
       console.log(`try to ping server ${serverUrl}`)
       serverTest.ping(TIMEOUT, PROTOCOL_VERSION, (err, res) => {
@@ -203,10 +194,8 @@ function onServerUpdate(url, err, res) {
 
   if (online === playerList.length && onlineInfo !== CACHED_STATUSES[url]?.online && playersInfo !== CACHED_STATUSES[url]?.players) {
     cacheStatus(url, onlineInfo, playersInfo)
-    cachePlayersTime(url, playerList)
 
-    const lastPlayers = getLastPlayers(url)
-    const text = `${url}\nonline: ${onlineInfo}${playersInfo ? `, players: ${playersInfo}` : ''}\nlast 24h: ${lastPlayers.join(', ')}`
+    const text = `${url}\nonline: ${onlineInfo}${playersInfo ? `, players: ${playersInfo}` : ''}`
 
     SERVERS_AND_CHATS_TO_NOTIFY[url].forEach(chatId => {
       updateStatusMessage(url, chatId, text)
@@ -218,7 +207,8 @@ setInterval(() => {
   Object.keys(SERVERS_AND_CHATS_TO_NOTIFY).forEach(url => {
     const subscribedChats = SERVERS_AND_CHATS_TO_NOTIFY[url]
     if (subscribedChats.length) {
-      const server = new mcping.MinecraftServer(url)
+      const [host, port] = url.split(':');
+      const server = new mcping.MinecraftServer(host, port)
       server.ping(TIMEOUT, PROTOCOL_VERSION, (err, res) => onServerUpdate(url, err, res))
     }
   })
