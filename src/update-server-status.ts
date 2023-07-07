bab4026fb7057e879a258fe678f20ca692c84dea
A77AY <11846445+A77AY@users.noreply.github.com>
2023-07-07 22:29:13 +0400

    fix

--- src/update-server-status.ts
+++ src/update-server-status.ts
@@ -21,31 +21,25 @@ function formatOnlinePlayer(player: PlayerStatus) {
 function formatOfflinePlayer(player: PlayerStatus) {
   const minutes = Math.abs(differenceInMinutes(player.lastOnline, new Date()));
   return `${player.name} ~ ${
-    minutes < 1 ? formatDuration({ minutes }) : "a few seconds"
+    minutes < 1 ? "a few seconds" : formatDuration({ minutes })
   } ago`;
 }
 
 export function getServerStatusMessage(url: string, status: ServerStatus) {
   if (!status) return "";
+  const offline = status.offline
+    .filter((p) => Math.abs(differenceInMinutes(p.lastOnline, new Date())) < 60)
+    .sort(
+      (a, b) => a.lastOnline.getMilliseconds() - b.lastOnline.getMilliseconds(),
+    );
   return `${url}\nOnline: ${status.online.length}/${status.server.max}${
-    status.online.length || status.offline.length
+    status.online.length || offline.length
       ? `\n\nPlayers:\n` +
         (status.online.sort((a, b) => a.name.localeCompare(b.name)).length
           ? status.online.map(formatOnlinePlayer).join("\n")
           : "-") +
-        (status.offline.length
-          ? `\n\n${status.offline
-              .filter(
-                (p) =>
-                  Math.abs(differenceInMinutes(p.lastOnline, new Date())) >= 60,
-              )
-              .sort(
-                (a, b) =>
-                  a.lastOnline.getMilliseconds() -
-                  b.lastOnline.getMilliseconds(),
-              )
-              .map(formatOfflinePlayer)
-              .join("\n")}`
+        (offline.length
+          ? `\n\n${offline.map(formatOfflinePlayer).join("\n")}`
           : "")
       : ""
   }`;
